f4b4f661f0835e2e47e9a8f3835c75fce685da5a
Mike <mike@catalystcode.io>
2022-09-08 02:03:02 -0500

    added debug mode

--- source/main.c
+++ source/main.c
@@ -23,10 +23,9 @@
 #endif
 #endif
 
-#define VERBOSE_LOGGING 0
-
 // Global State
 // --------------------
+int debug_enabled = false;
 u16 all_buttons_held;
 extern u8 __xfb[];
 // --------------------
@@ -46,6 +45,31 @@ void scan_all_buttons_held()
         PAD_ButtonsHeld(PAD_CHAN2) |
         PAD_ButtonsHeld(PAD_CHAN3)
     );
+}
+
+void wait_for_confirmation()
+{
+    // Wait until the A button or reset button is pressed.
+    int cur_state = true;
+    int last_state;
+    do
+    {
+        VIDEO_WaitVSync();
+        scan_all_buttons_held();
+        last_state = cur_state;
+        cur_state = all_buttons_held & PAD_BUTTON_A;
+    }
+    while (last_state || !cur_state);
+}
+
+void delay_exit()
+{
+    if (debug_enabled)
+    {
+        // When debug is enabled, always wait for confirmation before exit.
+        kprintf("\nDEBUG: Press A to continue...\n");
+        wait_for_confirmation();
+    }
 }
 
 void read_dol_file(u8 **dol_file, const char *path)
@@ -235,25 +259,6 @@ int load_usb(BOOT_PAYLOAD *payload, char slot)
     return res;
 }
 
-void delay_exit()
-{
-    // Wait while the d-pad down direction or reset button is held.
-    if (all_buttons_held & PAD_BUTTON_DOWN)
-    {
-        kprintf("(release d-pad down to continue)\n");
-    }
-    if (SYS_ResetButtonDown())
-    {
-        kprintf("(release reset button to continue)\n");
-    }
-
-    while (all_buttons_held & PAD_BUTTON_DOWN || SYS_ResetButtonDown())
-    {
-        VIDEO_WaitVSync();
-        scan_all_buttons_held();
-    }
-}
-
 int main()
 {
     VIDEO_Init();
@@ -288,6 +293,13 @@ int main()
     settime(secs_to_ticks(t));
 
     scan_all_buttons_held();
+
+    // Check if d-pad down direction or reset button is held.
+    if (all_buttons_held & PAD_BUTTON_DOWN || SYS_ResetButtonDown())
+    {
+        kprintf("DEBUG: Debug enabled.\n");
+        debug_enabled = true;
+    }
 
     if (all_buttons_held & PAD_BUTTON_LEFT || SYS_ResetButtonDown())
     {
@@ -336,23 +348,26 @@ int main()
     }
 
     // Print DOL args.
-#if VERBOSE_LOGGING
-    if (payload.argv.length > 0)
+    if (debug_enabled)
     {
-        kprintf("----------\n");
-        size_t position = 0;
-        for (int i = 0; i < payload.argv.argc; ++i)
+        if (payload.argv.length > 0)
         {
-            kprintf("arg%i: %s\n", i, payload.argv.commandLine + position);
-            position += strlen(payload.argv.commandLine + position) + 1;
+            kprintf("\nDEBUG: About to print CLI args. Press A to continue...\n");
+            wait_for_confirmation();
+            kprintf("----------\n");
+            size_t position = 0;
+            for (int i = 0; i < payload.argv.argc; ++i)
+            {
+                kprintf("arg%i: %s\n", i, payload.argv.commandLine + position);
+                position += strlen(payload.argv.commandLine + position) + 1;
+            }
+            kprintf("----------\n\n");
         }
-        kprintf("----------\n\n");
+        else
+        {
+            kprintf("DEBUG: No CLI args\n");
+        }
     }
-    else
-    {
-        kprintf("No CLI args\n");
-    }
-#endif
 
     // Prepare DOL argv.
     if (payload.argv.length > 0)

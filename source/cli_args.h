6987575903705363c7e5a9a6fd0786263ba781c1
Mike <mike@catalystcode.io>
2022-09-08 01:31:27 -0500

    better CLI arg parsing
    - Better parsing algo which ignores insignificant white space in CLI args.

--- source/cli_args.h
+++ source/cli_args.h
@@ -2,6 +2,6 @@
 #define INC_CLI_ARGS_H
 #include <gctypes.h>
 
-int parse_cli_args(struct __argv *argv, char *cli_option_str);
+int parse_cli_args(struct __argv *argv, const char *cli_option_str);
 
 #endif
